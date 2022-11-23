3c5c67a7af22a68f85fbbe1d6ada12822da39a5e
amiof <arm131313@gmail.com>
2022-11-23 14:42:47 +0330

    server configed for up

---
+++ app/server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app =express()
+const mongoose = require("mongoose")
+const http = require("http")
+class Application{
+    constructor(port,URL_DB){
+       this.configApplication()
+       this.configDatabase(URL_DB)
+       this.createServer(port)
+       this.createRoutes()
+       this.errorHandler()
+    }
+    configApplication(){
+        const path= require("path")
+        app.use(express.json())
+        app.use(express.urlencoded({extended: true}))
+        app.use(express.static(path.join(__dirname,"..","public")))
+
+
+
+
+    }
+    createServer(port){
+       const server= http.createServer(app)
+       server.listen(port,()=>{
+        console.log(`server > up in http://localhost:${port}`)
+            
+        })
+    }
+     
+    configDatabase(URL_DB){
+        mongoose.connect(URL_DB,(error)=>{
+            if(error) throw {status:400,message:error}
+            return console.log("connected to db successful")
+        })
+
+    }
+    errorHandler(){
+        app.use((req,res,next)=>{
+            return res.status(404).json({
+
+                status:404,
+                success:false,
+                message: "this page not found"
+            }
+            )
+        })
+        app.use((error, req,res,next)=>{
+            const status =error?.status||500
+            const message=error?.message|| "internal server error"
+            return res.status(status).json({
+                status,
+                message,
+            })
+        })
+    }
+    createRoutes(){
+        app.get("/",(req, res, next)=>{
+            return res.json({
+                message: "express server is up"
+            })
+        })
+    }
+
+}   
+module.exports=Application
---
+++ index.js
@@ -0,0 +1,5 @@
+const Application = require("./app/server")
+const PORT=3500
+const DB_URL="mongodb://127.0.0.1:27017/ProjectControllerDB"
+
+new Application(PORT,DB_URL )
