500f5dc702f6dd8a567cd0ebf127e94b301cafc0
amiof <arm131313@gmail.com>
2022-11-26 08:19:14 +0330

    add edit profile

--- app/http/controller/user.controller.js
+++ app/http/controller/user.controller.js
@@ -1,3 +1,5 @@
+const { userModel } = require("../../model/user")
+
 class UserController{
     getProfile(req,res,next){
         try{
@@ -12,8 +14,29 @@ class UserController{
         }
 
     }
-    editProfile(){
+  async  editProfile(req,res,next){
+        try {
+            let data=req.body
+            const fields=["first_name", "last_name", "skills"]
+            const userId=req.user._id
+            const badValue=[" ",null ,undefined,"",".","*",NaN]
+             Object.entries(data).forEach(([key,value]) => {
+                if(!fields.includes(key)) delete data[key]
+                if(badValue.includes(value)) delete data[key]
+                
+             });
+            console.log(data)
+            const result =await userModel.updateOne({_id:userId}, {$set:data})
+            if(Object.keys(data).length===0) throw {status:400, message:"بروزرسانی با انجام نشد"}
+            return res.status(200).json({
+                status:200,
+                success:true,
+                message:"بروزرسانی با موفقیت انجام شد"
+            })
 
+        } catch (error) {
+            next(error)
+        }
     }
     addSkills(){
 
--- app/routes/user.js
+++ app/routes/user.js
@@ -4,6 +4,7 @@ const { autoLogin } = require("../http/middleware/AutoLogin")
  const router=require("express").Router()
 
  router.get("/profile", autoLogin,UserController.getProfile)
+ router.post("/profile/edit", autoLogin,UserController.editProfile)
  
  module.exports={
     userRoutes:router
